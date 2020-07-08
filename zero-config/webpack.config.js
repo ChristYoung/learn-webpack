16bf33a63a8c3c514414ecd623ee825a792c7d35
yangjie <jie.yang@witontek.com>
2020-07-08 21:20:43 +0800

    update--使用html-webpack-plugin

--- zero-config/webpack.config.js
+++ zero-config/webpack.config.js
@@ -1,9 +1,18 @@
-var path = require("path")
+var path = require("path");
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['./src/js/main.js', './src/js/a.js'], // 打包的入口文件(多个入口文件打包到一起)
+    entry: {                                      // 1. 多个chunk打包
+        main: './src/js/main.js',
+        a: './src/js/a.js',
+    },
     output: {
         path: path.resolve(__dirname, './dist/js'), // 打包后的输出路径, 使用Node中的Path模块, 执行为绝对路径
-        filename: 'bundle.js' // 打包后的文件名称
+        filename: '[name]-[chunkhash].js', // 1. 多个chunk打包打包后的文件名称使用name和chunkhash占位符, name表示entry中的键值, chunkhash表示打包的hash
     },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ],
 };
