fd571aa7857b5664cf7dec40167c47ca605da669
yangjie <jie.yang@witontek.com>
2020-07-08 21:42:07 +0800

    update--

--- zero-config/webpack.config.js
+++ zero-config/webpack.config.js
@@ -1,5 +1,6 @@
-var path = require("path");
-var htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {                                      // 1. 多个chunk打包
@@ -7,12 +8,15 @@ module.exports = {
         a: './src/js/a.js',
     },
     output: {
-        path: path.resolve(__dirname, './dist/js'), // 打包后的输出路径, 使用Node中的Path模块, 执行为绝对路径
-        filename: '[name]-[chunkhash].js', // 1. 多个chunk打包打包后的文件名称使用name和chunkhash占位符, name表示entry中的键值, chunkhash表示打包的hash
+        path: path.resolve(__dirname, './dist'), // 打包后的输出路径, 使用Node中的Path模块, 执行为绝对路径
+        filename: 'js/[name]-[chunkhash].js', // 1. 多个chunk打包打包后的文件名称使用name和chunkhash占位符, name表示entry中的键值, chunkhash表示打包的hash
     },
     plugins: [
+        new webpack.ProgressPlugin(), // 在打包很慢的时候, 展示打包的进度条
         new htmlWebpackPlugin({
-            template: 'index.html'
+            filename: 'young.html',
+            template: 'index.html',
+            title: 'webpack is something awesome!!'
         })
     ],
 };
