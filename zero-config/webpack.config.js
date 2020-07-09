7366a698f259b288fec27225006e48f25170a0ff
yangjie <jie.yang@witontek.com>
2020-07-09 17:17:18 +0800

    update--安装使用css-loader和style-loader

--- zero-config/webpack.config.js
+++ zero-config/webpack.config.js
@@ -10,14 +10,16 @@ module.exports = {
         path: path.resolve(__dirname, './dist'), // 打包后的输出路径, 使用Node中的Path模块, 执行为绝对路径
         filename: 'js/[name]-[chunkhash].js', // 1. 多个chunk打包打包后的文件名称使用name和chunkhash占位符, name表示entry中的键值, chunkhash表示打包的hash
     },
-    // module: {
-    //     loaders: [
-    //         {
-    //             test: /\.js$/,
-    //             loader: 'babel',
-    //         }
-    //     ]
-    // },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: path.resolve(__dirname, './node_modules'), // 忽略打包node_modules中的js文件
+                include: path.resolve(__dirname, './src'), // 只打包src目录下的js文件
+                loader: 'babel-loader',
+            }
+        ]
+    },
     plugins: [
         new webpack.ProgressPlugin(), // 在打包很慢的时候, 展示打包的进度条
         new htmlWebpackPlugin({
