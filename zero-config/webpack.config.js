8501a28a105ed8dd8d53a7db3493881b53be87d6
yangjie <jie.yang@witontek.com>
2020-07-09 22:30:51 +0800

    update--

--- zero-config/webpack.config.js
+++ zero-config/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, './dist'), // 打包后的输出路径, 使用Node中的Path模块, 执行为绝对路径
         filename: 'js/[name]-[chunkhash].js', // 1. 多个chunk打包打包后的文件名称使用name和chunkhash占位符, name表示entry中的键值, chunkhash表示打包的hash
     },
+    // 以下是各种loader
     module: {
         rules: [
             {
