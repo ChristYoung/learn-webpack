462df39f3867d0098f23caa556550c992b628e02
yangjie <jie.yang@witontek.com>
2020-07-09 16:23:48 +0800

    update--尝试使用loader

--- zero-config/src/js/main.js
+++ zero-config/src/js/main.js
@@ -1,3 +1,14 @@
-function hello() {
-    return 'hello';
-}
+const hello = () => {
+    console.log('hello~');
+    alert('hello!');
+};
+
+const Main = () => {
+    console.log('this is Main');
+    const s = Object.assign({}, new Date());
+    console.log('sss', s);
+    alert('main is this!!');
+};
+
+hello();
+Main();
--- zero-config/webpack.config.js
+++ zero-config/webpack.config.js
@@ -5,12 +5,19 @@ const webpack = require('webpack');
 module.exports = {
     entry: {                                      // 1. 多个chunk打包
         main: './src/js/main.js',
-        a: './src/js/a.js',
     },
     output: {
         path: path.resolve(__dirname, './dist'), // 打包后的输出路径, 使用Node中的Path模块, 执行为绝对路径
         filename: 'js/[name]-[chunkhash].js', // 1. 多个chunk打包打包后的文件名称使用name和chunkhash占位符, name表示entry中的键值, chunkhash表示打包的hash
     },
+    // module: {
+    //     loaders: [
+    //         {
+    //             test: /\.js$/,
+    //             loader: 'babel',
+    //         }
+    //     ]
+    // },
     plugins: [
         new webpack.ProgressPlugin(), // 在打包很慢的时候, 展示打包的进度条
         new htmlWebpackPlugin({
