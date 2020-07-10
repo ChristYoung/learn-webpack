0619d5569e58afbfe6db2121b12dd7959a022022
yangjie <jie.yang@witontek.com>
2020-07-10 14:25:19 +0800

    update--less loader

--- zero-config/src/js/main.js
+++ zero-config/src/js/main.js
@@ -1,5 +1,6 @@
-// 在webpack中任何类型的文件都可以当成是一个模块引入进来, 比如此处引入了一个css文件
+// 在webpack中任何类型的文件都可以当成是一个模块引入进来, 比如此处引入了一个css文件和less文件
 import '../styles/common.css';
+import '../styles/main.less';
 
 const hello = () => {
     console.log('hello~');
