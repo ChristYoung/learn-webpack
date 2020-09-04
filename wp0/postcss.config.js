cc7344bc15f19c4fa7dccb3308d4812ec9ed61ce
ChristYoung <858851768@qq.com>
2020-09-04 14:00:53 +0800

    delete--

--- wp0/postcss.config.js
+++
@@ -1,6 +0,0 @@
-// https://newsn.net/say/webpack-postcss.html
-module.exports = {
-    plugins: [
-        require('autoprefixer') // autoprefixer可以将正在实验中的css属性加上各个浏览器前缀, 如现在index.html中的::placeholder属性
-    ]
-};
--- wp0/src/js/a.js
+++
@@ -1,5 +0,0 @@
-function a() {
-    console.log('a');
-    console.log('检查是否开启了webpack  HMR功能~');
-}
-a();
